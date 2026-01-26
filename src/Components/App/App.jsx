80d42d1cfb2276fa2d85660d21103720d8c7a32e
Kaich1k <Kaigr7@gmail.com>
2026-01-26 17:26:17 -0600

    Second Commit

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 id="title">Jammming</h1>
+    </div>
+  )
+}
+
+export default App;
