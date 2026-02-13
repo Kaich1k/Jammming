818a37421d789fe4bfaf8f4d56af3ab71816c4c1
Kaich1k <Kaigr7@gmail.com>
2026-02-12 22:18:54 -0600

    test 6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Jammming/',  // required for GitHub Pages: https://kaich1k.github.io/Jammming/
+  envDir: __dirname,   // load .env from folder containing vite.config.js (project root)
 })
