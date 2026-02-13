ecadffad3c6316e0633457476c916a214386e913
Kaich1k <Kaigr7@gmail.com>
2026-02-12 19:09:31 -0600

    Github Pages Fix #2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Jammming/',  // required for GitHub Pages: https://kaich1k.github.io/Jammming/
 })
